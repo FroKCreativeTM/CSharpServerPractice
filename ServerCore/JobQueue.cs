bb85cd5c964e5b74216b36f02cc9c3aa94687241
frokcreative <cscculture95@gmail.com>
2022-03-28 00:43:33 +0900

    잡 큐 작업 시작

---
+++ ServerCore/JobQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    public interface IJobQueue
+    {
+        void Push(Action job);
+    }
+
+    class JobQueue : IJobQueue
+    {
+        Queue<Action> _jobQueue = new Queue<Action>();
+        object _jobQueueLock = new object();
+
+        public void Push(Action job)
+        {
+            lock(_jobQueueLock)
+                _jobQueue.Enqueue(job);
+        }
+
+        Action Pop()
+        {
+            lock(_jobQueueLock)
+            {
+                if (_jobQueue.Count == 0)
+                    return null;
+                return _jobQueue.Dequeue();
+            }
+        }
+    }
+}
